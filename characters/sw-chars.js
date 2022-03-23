b6bb10f4b3eab5724d1b22c870fe6b7cb330d100
Jackson <jackson.tuttle.4@gmail.com>
2022-03-22 20:44:02 -0600

    css good stuff

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -11,25 +11,41 @@ allCharsButton.addEventListener("click", function (event) {
   populateDOM(people);
 });
 
-
-const maleCharacters = people.filter(person => person.gender === 'male')
+const maleCharacters = people.filter((person) => person.gender === "male");
 
 // TODO: make a filter for female chars
 // TODO: make a filter for female chars
 
-const maleCharsButton = document.createElement('button')
-maleCharsButton.textContent  = 'Male Characters'
-maleCharsButton.addEventListener('click', () => populateDOM(maleCharacters))
+const maleCharsButton = document.createElement("button");
+maleCharsButton.textContent = "Male Characters";
+maleCharsButton.addEventListener("click", () => populateDOM(maleCharacters));
+
+const otherCharactersButton = document.createElement("button");
+otherCharactersButton.textContent = "Other Characters";
+otherCharactersButton.addEventListener("click", () =>
+  populateDOM(otherCharacters)
+);
 
 // TODO: create a female chars button and add ot the dom
 // TODO: create a other chars button and add ot the dom
 
+const otherCharacters = people.filter((person) => {
+  if (
+    person.gender === "hermaphrodite" ||
+    person.gender === "none" ||
+    person.gender === "n/a"
+  ) {
+    return person;
+  }
+});
+
+header.appendChild(otherCharactersButton);
 header.appendChild(allCharsButton);
-header.appendChild(maleCharsButton)
+header.appendChild(maleCharsButton);
 
 function populateDOM(characters) {
   //loop through all the characters and make figure elements and insert them into the DOM
-  removeChildren(main)
+  removeChildren(main);
   characters.forEach((person) => {
     const personFig = document.createElement("figure");
     const personImg = document.createElement("img");
@@ -44,6 +60,6 @@ function populateDOM(characters) {
     personFig.appendChild(personCap);
     main.appendChild(personFig);
   });
-} 
+}
 
-populateDOM(people)
+populateDOM(people);
