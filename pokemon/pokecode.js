a940ca50b9e51a7ed2b14c4ff38611dc84da95fc
Jackson <jackson.tuttle.4@gmail.com>
2022-04-05 11:16:46 -0600

    initial commit for pokemon

---
+++ pokemon/pokecode.js
@@ -0,0 +1,15 @@
+
+
+function getAPIData(url) {
+    try {
+        return fetch(url).then((data) => data.json())
+    } catch (error) {
+    console.error(error)
+    }
+}
+
+function loadPokemon() {
+    getAPIData('https://pokeapi.co/api/v2/pokemon/snorlax').then(
+        (data => console.log(data))
+    )
+}
